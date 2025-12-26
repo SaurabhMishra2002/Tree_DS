aade47b09b4e673cb2d3f99025405c127f36dd4d
Lenovo <Saurabh2002jpl@gmail.com>
2025-12-26 11:55:43 +0530

    ModifiedBST

--- BSTEx1.java
+++ BSTEx1.java
@@ -12,6 +12,76 @@ public class BSTEx1 {
         }
     }
 
+    public static int size(Node node){
+        if(node == null){
+            return 0;
+        }
+        int ls = size(node.left);
+        int rs = size(node.right);
+        return ls + rs + 1;
+    }
+
+    public static int  height(Node node){
+        if(node == null){
+            return -1;
+        }
+
+        int lh = height(node.left);
+        int rh = height(node.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+
+    public static int sum(Node node){
+        if(node == null){
+            return 0;
+        }
+
+        int ls = sum(node.left);
+        int rs = sum(node.right);
+
+        return ls + rs + node.data;
+    }
+
+ public static int maxBST(Node node){
+    if(node.right != null){
+        return maxBST(node.right);
+    }else{
+        return node.data;
+    }
+
+ }
+
+ public static int minBST(Node node){
+    if(node.left != null){
+        return minBST(node.left);
+    }else{
+        return node.data;
+    }
+ }
+
+//How to find node in BST
+    public static boolean find(Node node, int data){
+
+        if(node == null){
+            return false;
+        }
+
+        if(data < node.data){
+            return find(node.left, data);
+        }else if(data > node.data){
+            return find(node.right, data);
+        }else{
+            return true;
+        }
+    }
+
+    public static void insert(Node node , int data){
+        
+    }
+
+
 
     public static Node buildBST(int []arr,int lo, int hi){
         if(lo > hi){
